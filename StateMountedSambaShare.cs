0e74358fc7143491c09b16b8e44a761105099af3
David Aslan <dev@davidaslan.se>
2022-07-28 21:43:34 +0200

    Handle return from bash in string builder

--- StateMountedSambaShare.cs
+++ StateMountedSambaShare.cs
@@ -1,3 +1,5 @@
+﻿using System;
+using System.Text;
 using System.Threading.Tasks;
 using CliWrap;
 using CliWrap.Buffered;
@@ -8,6 +10,9 @@ namespace StateMountedSambaShare
     {
         static async Task Main(string[] args)
         {
+            var stdOutBuffer = new StringBuilder();
+            var stdErrBuffer = new StringBuilder();
+
             var result = await Cli.Wrap("findmnt")
                 .WithArguments("-t cifs,nfs4")
             //    .WithWorkingDirectory("work/dir/path")
@@ -15,6 +20,20 @@ namespace StateMountedSambaShare
                 .WithStandardErrorPipe(PipeTarget.ToStringBuilder(stdErrBuffer))
 
                 .ExecuteBufferedAsync();
+
+            // Contains stdOut/stdErr buffered in-memory as string
+            var stdOut = stdOutBuffer.ToString();
+            var stdErr = stdErrBuffer.ToString();
+
+             if (stdOut.Contains("")) {
+                Console.WriteLine("We found your mounted samba share!");
+            }
+            else
+            {
+                Console.WriteLine("There is no mounted samba share.");
+
+            }            
+
         }
 
     }
